d2507a06576fe29d7a2a5205aaa7e9b4bccb24d8
dorraturki <56479877+dorraturki@users.noreply.github.com>
2019-12-22 12:18:28 +0100

    Add files via upload

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -94,24 +94,7 @@ jQuery(document).ready(function($) {
     if( ! action ) {
       action = 'contactform/contactform.php';
     }
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
 
-      }
-    });
     return false;
   });
 
